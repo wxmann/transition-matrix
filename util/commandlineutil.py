d58f43c47541c6db359c0435158181adc3715cd4
wxmann <wxfreakk91@gmail.com>
2015-04-19 01:33:41 -0700

    Accidentally erased a function in this file

--- util/commandlineutil.py
+++ util/commandlineutil.py
@@ -3,6 +3,10 @@ __author__ = 'tangz'
 _PERIOD_RANGE_INDICATOR = ":"
 _COMMA_SEPARATOR = ","
 
+def parse_state_str(states_str):
+    split_strs = states_str.strip().lstrip('[').rstrip(']').split(_COMMA_SEPARATOR)
+    return [state_str.strip() for state_str in split_strs]
+
 def parse_periods(periods_str):
     if _PERIOD_RANGE_INDICATOR in periods_str:
         return _parse_period_range(periods_str)
